726971ec69dfba001e7f05d64dabc2941f448b8b
senugbin9850 <tanja1206@naver.com>
2021-04-22 10:00:08 +0900

    [ADD] config

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import { config } from 'dotenv';
+
+config();
+
+export default {
+  database: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  },
+};
